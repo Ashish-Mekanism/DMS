e71d5a395ce7625228be0539f08b2edf7b08cf95
Ashish-Mekanism <ashish.dev@tech.mekanism.cc>
2025-06-18 15:21:34 +0530

    refactor: clean up code formatting and update database connection settings

--- utils/apiDb.js
+++ utils/apiDb.js
@@ -1,34 +1,22 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-// const pool = mysql.createPool({
-//   host: process.env.API_DB_HOST,
-//   user: process.env.API_DB_USER,
-//   password: process.env.API_DB_PASSWORD,
-//   database: process.env.API_DB_DATABASE,
-//   port: process.env.API_DB_PORT,
-//   waitForConnections: true,
-//   //connectionLimit: process.env.DB_POOL_MAX,
-//   queueLimit: 0,
-//   dateStrings: true
-// });
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  host: process.env.API_DB_HOST,
+  user: process.env.API_DB_USER,
+  password: process.env.API_DB_PASSWORD,
+  database: process.env.API_DB_DATABASE,
+  port: process.env.API_DB_PORT,
   waitForConnections: true,
-  connectionLimit: 10,
+  //connectionLimit: process.env.DB_POOL_MAX, 
   queueLimit: 0,
-  dateStrings: true
+  dateStrings: true,
 });
 
 // Check if the pool has successfully connected
 pool.getConnection((err, connection) => {
   if (err) {
-    console.log(" ===>", err);
-    console.error("Error connecting to MySQL:", err);
+    console.error(" not Error connecting to MySQL:", err);
     return;
   }
   console.log("API MySQL connected successfully!");
