d9c24f7e34ae62f5c0e03b42509e335a0351e8e5
Ashish-Mekanism <ashish.dev@tech.mekanism.cc>
2025-06-23 10:46:57 +0530

    style: format code for improved readability in GST registration routes

--- routes/gstRegistration.routes.js
+++ routes/gstRegistration.routes.js
@@ -1,12 +1,13 @@
+// this file defines the routes for GST registration operations
 const express = require("express");
 const router = express.Router();
 const gstRegistrationController = require("../controllers/gstregistration.controller");
 const authToken = require("../middlewares/authenticateToken");
 
-router.post("/",authToken, gstRegistrationController.create);
-router.get("/",authToken, gstRegistrationController.findAll);
-router.get("/:id",authToken ,gstRegistrationController.findOne);
-router.put("/:id",authToken, gstRegistrationController.update);
-router.delete("/:id",authToken, gstRegistrationController.delete);
+router.post("/", authToken, gstRegistrationController.create);
+router.get("/", authToken, gstRegistrationController.findAll);
+router.get("/:id", authToken, gstRegistrationController.findOne);
+router.put("/:id", authToken, gstRegistrationController.update);
+router.delete("/:id", authToken, gstRegistrationController.delete);
 
 module.exports = router;
